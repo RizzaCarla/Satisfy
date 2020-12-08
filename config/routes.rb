5448e31f0a33a05f8905951a9ea78a0cd157d28e
rizzacarla <rizzacarla.12@gmail.com>
2020-12-08 11:40:58 -0800

    Add static page in the controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/root'
+  get 'static/pages'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update, :destroy, :show]
