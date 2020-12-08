86cc76ded058dfb5e0467f37b756ddbf5396da1e
rizzacarla <rizzacarla.12@gmail.com>
2020-12-08 13:56:08 -0800

    Seed the database and extracted Json Object from views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all!
+
+# Satisfy Users
+
+rizza = User.create!(username: 'RizzaCarla12', email: 'rizzacarla_12@satisfy.com', password: 'satisfyowner')
+dhiveshan = User.create!(username: 'Dave', email: 'dc@gmail.com', password: '1231234567812')
+shane = User.create!(username: 'PunnyShane', email: 'BestBoss@theoffice.com', password: 'Sharrrrr')
+izzi = User.create!(username: 'DizIzIzzie', email: 'IzziIsTooCool@foryou.com', password: 'Isabelle')
+jae = User.create!(username: 'DaddyJae', email: 'IamDaddy@tinder.com', password: 'CallMeDaddy')
+san = User.create!(username: 'SanSoSweet', email: 'SanSugarCane@candy.com', password: 'ImAnAngelInDisguise')
