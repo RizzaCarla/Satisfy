dcfd7a8d9226712ebb52add989a07e814becf46e
rizzacarla <rizzacarla.12@gmail.com>
2020-12-08 20:39:13 -0800

    Remove {} around logger and thunk imports

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root';
-import { logger } from 'redux-logger';
-import { thunk } from 'redux-thunk';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 const configureStore = (preloadedState = {}) => (
   createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
