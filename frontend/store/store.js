cfb59e1e59ce511f7f4108e60b7794c3f7c40964
rizzacarla <rizzacarla.12@gmail.com>
2020-12-08 20:55:26 -0800

    Change imports to find the bug

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
+// import thunk from 'redux-thunk';
+import React from 'react'
 
 const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk))
 )
 
 export default configureStore;
