5bd28703abdf322a059525ba8c1639f81eb59ed4
rizzacarla <rizzacarla.12@gmail.com>
2020-12-12 20:21:01 -0800

    Add index jbuilder for songs

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,6 +1,11 @@
 class Api::SongsController < ApplicationController
   def show
     @song = Song.find_by(id: params[:id])
-    render :json "api/songs/show"
+    render json: "api/songs/show"
+  end
+
+  def index
+    @songs = Songs.all
+    render json: "api/songs/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update, :destroy, :index, :show]
     resource :session, only: [:create, :destroy]
+    resources :songs, only: [:show, :index]
   end
 
   root to: 'static_pages#root';
