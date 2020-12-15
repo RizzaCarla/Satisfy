04c06e3319412da345f3328c663d2e5842be567e
rizzacarla <rizzacarla.12@gmail.com>
2020-12-15 15:34:06 -0800

    Rename reducer

--- frontend/components/music-player/music_player_container.js
+++ frontend/components/music-player/music_player_container.js
@@ -5,8 +5,8 @@ const msp = state => ({
   song: state.entities.songs[state.session.id]
 })
 
-// const mdp = dispatch => ({
+const mdp = dispatch => ({
 
-// })
+})
 
 export default connect(msp, null)(MusicPlayer)
