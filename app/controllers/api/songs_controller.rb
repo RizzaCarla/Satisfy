89ffcc605bef3ec0fcb96c9f79f39322bc5ad560
rizzacarla <rizzacarla.12@gmail.com>
2020-12-16 15:36:02 -0800

    Remove merge conflict in controller

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,12 +1,7 @@
 class Api::SongsController < ApplicationController
   def show
-<<<<<<< Updated upstream
-    @song = Song.with_attached_song.find_by(id: params[:id)
-    render json: "/api/users/show"
-=======
     @song = Song.with_attached_songUrl.find_by(id: params[:id])
     render "/api/songs/show"
->>>>>>> Stashed changes
   end
 
   def index
