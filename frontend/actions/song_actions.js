bafb4869962ee9bfcf2c7f688edf3a18fa19f70c
rizzacarla <rizzacarla.12@gmail.com>
2020-12-17 01:23:44 -0800

    Add action creators for pause and play

--- frontend/actions/song_actions.js
+++ frontend/actions/song_actions.js
@@ -1,12 +1,24 @@
-import * as songAPIUtil from '../util/api_song_util'
+import * as songAPIUtil from '../util/api_song_util';
 
-export const RECEIVE_SONGS = 'RECEIVE_SONGS'
-export const RECEIVE_SONG = 'RECEIVE_SONG'
-export const CHANGE_CURRENT_SONG = 'CHANGE_CURRENT_SONG'
+export const RECEIVE_SONGS = 'RECEIVE_SONGS';
+export const RECEIVE_SONG = 'RECEIVE_SONG';
+export const CHANGE_CURRENT_SONG = 'CHANGE_CURRENT_SONG';
+export const CHECK_PLAY = 'CHECK_PLAY';
+export const CHECK_PAUSE = 'CHECK_PAUSE';
 
 export const changeCurrentSong = (songId) => ({
   type: CHANGE_CURRENT_SONG,
   songId
+})
+
+export const checkPlay = (time) => ({
+  type: CHECK_PLAY,
+  time
+})
+
+export const checkPause = (time) => ({
+  type: CHECK_PAUSE,
+  time
 })
 
 export const receiveSongs = songs => ({
