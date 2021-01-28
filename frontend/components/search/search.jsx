4d06fd7527fd68662dce45e9d9d4332cd4a98856
rizzacarla <rizzacarla.12@gmail.com>
2021-01-28 09:40:41 -0800

    Change the search to show search result

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import SongIndexContainer from '../song/song_index_container';
+// import ArtistIndexContainer from '../artist/artist_index_container';
+// import AlbumIndexContainer from '../album/album_index_container';
+
 
 class Search extends React.Component {
   render() {
     return(
-      <div className='search'>
-        <input 
-          type='text'
-          placeholder='Search for Artists, Songs, or Podcasts'
-        />
+      <div className="search-results">
+
+        <SongIndexContainer />
+        {/* <ArtistIndexContainer /> */}
+        {/* <AlbumIndexContainer /> */}
       </div>
     )
   }
