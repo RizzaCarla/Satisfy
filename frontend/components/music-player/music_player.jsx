73982616638c31ffdb93db5dcc79466ba8b0576e
rizzacarla <rizzacarla.12@gmail.com>
2021-01-28 09:37:06 -0800

    Add play button to music player and get rid of prebuilt controls

--- frontend/components/music-player/music_player.jsx
+++ frontend/components/music-player/music_player.jsx
@@ -2,113 +2,54 @@ import React from 'react';
 import { withRouter } from 'react-router';
 
 class MusicPlayer extends React.Component {
-  componentDidMount() {
-    this.props.fetchSong(this.props.songId);
-  }
-  
-  handleSplash() {
-    if (this.props.location.pathname != "/us") {
-      if (this.props.songId != null) {
-        return <audio src={`${this.props.songs[this.props.songId].songUrl}`} type="audio/mp3" controls autoPlay />
-      } if (this.props.songId === null) {
-        return <audio src="" type="audio/mp3" controls />
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.handlePlay = this.handlePlay.bind(this)
   }
 
+  componentDidMount() {
+    this.props.fetchSongs()
+  }
+
+  handlePlay() {
+    const audio = document.getElementById('audio')
+    const pButton = document.getElementById('pButton')
+     if (audio.paused) {
+      audio.play()
+      pButton.innerHTML = '&#xe035;'
+    } else {
+      audio.pause()
+      pButton.innerHTML = '&#xe038;'
+    }
+  }
+  
   render() {
-    return (
-      <div className="musicPlayer">
-        {this.handleSplash()}
-      </div>
-    )
+
+    if (this.props.location.pathname != "/us") {
+      if (this.props.songId != null) {
+        return (
+          <div className="musicPlayer">
+            <div className='pButton-container'>
+              <i id='pButton' onClick={this.handlePlay} className="material-icons">&#xe038;</i>
+            </div>
+            <audio id='audio' src={`${this.props.songs[this.props.songId].songUrl}`} type="audio/mp3" autoPlay/>
+          </div>
+        )
+      } if (this.props.songId === null) {
+        return (
+          <div className="musicPlayer">
+            <div className='pButton-container'>
+              <i id='pButton' onClick={this.handlePlay} className="material-icons">&#xe038;</i>
+            </div>
+            <audio id='audio'src="" type="audio/mp3" autoPlay/>
+          </div>
+        )
+      }
+    } else {
+      return null
+    }
   }
 }
 
 
 export default withRouter(MusicPlayer);
-
-
-// import React from 'react';
-// import { withRouter } from 'react-router';
-// class MusicPlayer extends React.Component {
-//   render() {
-//     // if I have current song and I'm on the songs index page, render the ff:
-//     if ((this.props.songId != null) && (this.props.location.pathname != "/us")) {         
-//         return(
-//           <div className="musicPlayer">
-//             <audio 
-//             ref={(audio) => {this.audio = audio;}}
-//             src={`${this.props.songs[this.props.songId].songUrl}`} 
-//             type="audio/mp3" 
-//             controls
-//             autoPlay
-//             />
-//           </div>
-//         )
-//       } 
-
-//     // if I do not have a currentsong, and I'm on the songs index page, render the ff:
-//     if ((this.props.songId === null) && (this.props.location.pathname != "/us")) {
-//         return (
-//           <div className="musicPlayer">
-//           <audio src="" type="audio/mp3" controls />
-//         </div>
-//       )
-//     }
-
-//     // if I have a current song and I'm on the splash page, render nothing
-//     // if ((this.props.songId !== null) && (this.props.location.pathname === "/us")) {         
-//       return null;
-//     // }
-//   }
-// }
-
-// export default withRouter(MusicPlayer);
-
-
-// constructor(props) {
-  //   super(props);
-  //   this.setState = {
-    //     playStatus: false,
-    //     // currentSongUrl: this.props.song.songUrl
-    //   }
-    //   this.handlePausePlay = this.handlePausePlay.bind(this);
-    // }
-    
-    // play song
-    // save audio tag to a variable
-    // access this.audio.play*
-    
-    // handlePausePlay() {
-      
-      //   if (!this.state.playStatus) {
-        //     // if this button has been clicked, then the playStatus is true, pause the song
-        //     this.setState({ playStatus: false })
-        //     this.audio.pause()
-        //   }
-        
-        //   if (this.state.playStatus) {
-          //     // if this button has been clicked, then the playStatus is false, play the song
-          //     this.setState({ playStatus: true })
-          //     this.audio.play()
-          //   }
-          
-          // } 
-          
-          // if (!this.state.playStatus) {
-            // <div className="musicPlayer">
-            //   {/* returns pause logo. song is playing*/}
-            //   <i className="material-icons" onClick={() => this.handlePausePlay()}>&#xe036;</i>
-            // </div>
-            // } else if (this.state.playStatus) {
-              //   return (
-                //     <div className="musicPlayer">
-                //       {/* returns pause logo. song is playing*/}
-                //       <i className="material-icons" onClick={() => this.handlePausePlay()}>&#xe039;</i>
-                //     </div>
-                //   )
-                // }
-                // }
-                
-                
