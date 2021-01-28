a5c3ae6e174d0fb1267d5728d12e8eaebb1d6cf4
rizzacarla <rizzacarla.12@gmail.com>
2021-01-28 09:39:58 -0800

    Change the navbar so that it is a class component instead of a functional component and have a navbar for each page

--- frontend/components/nav-bar/navbar.jsx
+++ frontend/components/nav-bar/navbar.jsx
@@ -1,66 +1,195 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router';
 
-const NavBar = ({user, logout}) => {
-  const loggedIn = () => (
-    <div className="app-header-loggedin">
-      {/* <div className="loggedin-navigation">
-        <div className="backward">
-        <i className='fas'>&#xf104;</i>
+class NavBar extends React.Component {
+  
+  handleLoggedIn() {
+    if (this.props.location.pathname === "/search") {
+      return (
+        <div className="app-header-loggedin-search">
+          <div className="loggedin-navigation">
+            <div className="backward">
+              <i className='fas'>&#xf104;</i>
+            </div>
+            <div className="forward">
+              <i className='fas'>&#xf105;</i>
+            </div>
+          </div>
+          <nav className='session-navbar-loggedin-search'>
+            <div className='search-navbar'>
+              <i id='magnifying-glass-search' className="material-icons">&#xe8b6;</i>
+              <input
+                className='search-input'
+                type='text'
+                placeholder='Search for Artists, Songs, or Albums'>
+              </input>
+              {/* <span>
+                
+              </span> */}
+            </div>
+            <div className='search-navbar-right'>
+              <li>
+                <a href='https://github.com/RizzaCarla' target="_blank" >Github</a>
+              </li>
+              <li>
+                <a href='https://www.linkedin.com/in/rizzamarzo/' target="_blank" >LinkedIn</a>
+              </li>
+            {/*<a href=''>Resume</a> */}
+              <li>
+                <Link to='/login' onClick={this.props.logout}>Logout</Link>
+              </li>
+            </div>
+          </nav>
         </div>
-        <div className="forward">
-        <i className='fas'>&#xf105;</i>
+      )
+    } else if (this.props.location.pathname === "/us") {
+      return (
+        <div className="app-header-loggedin-splash">
+          <Link to='/us'>
+          <div className="loggedin-logo">
+              <img id="fullLogo-splash" src={window.fullLogo} />
+          </div>
+          </Link>
+          <nav className='session-navbar-loggedin'>
+            <li>
+              <a href='https://github.com/RizzaCarla' target="_blank" >Github</a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/rizzamarzo/' target="_blank" >LinkedIn</a>
+            </li>
+            {/*<a href=''>Resume</a> */}
+            <li>
+              <Link to='/login' onClick={this.props.logout}>Logout</Link>
+            </li>
+          </nav>
         </div>
-      </div> */}
-      <Link to='/us'>
-        <div className="loggedin-logo">
-              <img id="fullLogo" src={window.fullLogo} />
+      )
+    } else if (this.props.location.pathname === "/home") {
+      return (
+        <div className="app-header-loggedin-home">
+          <div className="loggedin-navigation">
+            <div className="backward">
+              <i className='fas'>&#xf104;</i>
+            </div>
+            <div className="forward">
+              <i className='fas'>&#xf105;</i>
+            </div>
+          </div>
+          <nav className='session-navbar-loggedin-home'>
+            <li>
+              <a href='https://github.com/RizzaCarla' target="_blank" >Github</a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/rizzamarzo/' target="_blank" >LinkedIn</a>
+            </li>
+            {/*<a href=''>Resume</a> */}
+            <li>
+              <Link to='/login' onClick={this.props.logout}>Logout</Link>
+            </li>
+          </nav>
         </div>
-      </Link>
-      <nav className='session-navbar-loggedin'>
-        <li>
-          <a href='https://github.com/RizzaCarla'>Github</a>
-        </li>
-        <li>
-          <a href='https://www.linkedin.com/in/rizzamarzo/'>LinkedIn</a>
-        </li>
-        {/*<a href=''>Resume</a> */}
-        <li>
-          <p className='separator'> | </p>
-        </li>
-        <li>
-          <Link to='/login' onClick={logout}>Logout</Link>
-        </li>
-      </nav>
-    </div>
-  );
+      )
+    } else if (this.props.location.pathname === "/library") {
+      return (
+        <div className="app-header-loggedin">
+          <div className="loggedin-navigation">
+            <div className="backward">
+              <i className='fas'>&#xf104;</i>
+            </div>
+            <div className="forward">
+              <i className='fas'>&#xf105;</i>
+            </div>
+          </div>
+          <nav className="library-navbar">
+            <li>Artists</li>
+            <li>Albums</li>
+            <li>Playlists</li>
+          </nav>
+          <nav className='session-navbar-loggedin'>
+            <li>
+              <a href='https://github.com/RizzaCarla' target="_blank" >Github</a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/rizzamarzo/' target="_blank" >LinkedIn</a>
+            </li>
+            {/*<a href=''>Resume</a> */}
+            <li>
+              <Link to='/login' onClick={this.props.logout}>Logout</Link>
+            </li>
+          </nav>
+        </div>
+      )
+    } else {
+      return (
+        <div className="app-header-loggedin">
+          <div className="loggedin-navigation">
+            <div className="backward">
+              <i className='fas'>&#xf104;</i>
+            </div>
+            <div className="forward">
+              <i className='fas'>&#xf105;</i>
+            </div>
+          </div>
+          <nav className='session-navbar-loggedin'>
+            <li>
+              <a href='https://github.com/RizzaCarla' target="_blank" >Github</a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/rizzamarzo/' target="_blank" >LinkedIn</a>
+            </li>
+            {/*<a href=''>Resume</a> */}
+            <li>
+              <Link to='/login' onClick={this.props.logout}>Logout</Link>
+            </li>
+          </nav>
+        </div>
+      )
+      
+    }
+  }
 
-  const loggedOut = () => (
-    <div className="app-header-loggedout">
-      <Link to='/us'>
-          <img id="fullLogo" src={window.fullLogo} />
-      </Link>
-      <nav className='session-navbar-loggedout'>
-        <li>
-          <a href='https://github.com/RizzaCarla'>Github</a>
-        </li>
-        <li>
-          <a href='https://www.linkedin.com/in/rizzamarzo/'>LinkedIn</a>
-        </li>
-        <li>
-          <p className='separator'> | </p>
-        </li>
-        <li>
-          <Link to='/signup'>Sign Up</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        {/* <a href=''>Resume</a> */}
-      </nav>
-    </div>
-  );
-  return user ? loggedIn() : loggedOut()
+  handleLoggedOut() {
+    return (
+      <div className = "app-header-loggedout" >
+        <Link to='/us'>
+            <img id="fullLogo" src={window.fullLogo} />
+        </Link>
+        <nav className='session-navbar-loggedout'>
+          <li>
+            <a href='https://github.com/RizzaCarla' target="_blank" >Github</a>
+          </li>
+          <li>
+            <a href='https://www.linkedin.com/in/rizzamarzo/' target="_blank" >LinkedIn</a>
+          </li>
+          <li>
+            <Link to='/signup'>Sign Up</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          {/* <a href=''>Resume</a> */}
+        </nav>
+      </div>
+    )
+  }
+
+  render() {
+    if (this.props.user) {
+      return (
+        <div>
+          {this.handleLoggedIn()}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          {this.handleLoggedOut()}
+        </div>
+      )
+    }
+  }
+
 }
 
-export default NavBar;
+export default withRouter(NavBar)
