b2c27a7454cf7fd12053b3e350e0201784cb840b
rizzacarla <rizzacarla.12@gmail.com>
2021-02-01 15:56:16 -0800

    Change the div for the library to work with the aesthetics of the website

---
+++ frontend/components/library/library.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Library extends React.Component {
+  render() {
+    return (
+      <div className='main-library-container'>
+        {/* <div className='library-mock-sidebar'></div> */}
+        <div className='library-container'>
+          <h1>testing testing</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Library
---
+++ frontend/components/library/library_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Library from './library'
+
+const msp = state => {
+
+}
+
+const mdp = dispatch => {
+
+}
+
+export default connect(msp, mdp)(Library)
