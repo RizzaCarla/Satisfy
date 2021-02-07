12e421c5c173a74440879a0e515698c47ee09692
rizzacarla <rizzacarla.12@gmail.com>
2021-02-06 18:58:04 -0800

    Test something on home

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -2,32 +2,40 @@ import React from 'react';
 import SongIndexContainer from '../song/song_index_container';
 import ArtistIndexContainer from '../artist/artist_index_container';
 import AlbumIndexContainer from '../album/album_index_container';
+import SideBarContainer from '../side-bar/sidebar_container';
+import MusicPlayerContainer from '../music-player/music_player_container';
+import NavBarContainer from '../nav-bar/navbar_container';
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div className='home-page-container'>
-        <div className='inner-home-page-container'>
-          <div className='discover-container'>
-            <h1>Discover New Songs</h1>
-            <div className='discover-index'>
-              <SongIndexContainer />
+      <div>
+        <NavBarContainer />
+        <SideBarContainer />
+        <MusicPlayerContainer />
+        <div className='home-page-container'>
+          <div className='inner-home-page-container'>
+            <div className='discover-container'>
+              <h1>Discover New Songs</h1>
+              <div className='discover-index'>
+                <SongIndexContainer />
+              </div>
             </div>
-          </div>
-          <div className='discover-container'>
-            <h1>Discover New Artists</h1>
-            <div className='discover-index'>
-              <ArtistIndexContainer />
+            <div className='discover-container'>
+              <h1>Discover New Artists</h1>
+              <div className='discover-index'>
+                <ArtistIndexContainer />
+              </div>
             </div>
-          </div>
-          <div className='discover-container'>
-            <h1>Discover New Albums</h1>
-            <div className='discover-index'>
-              <AlbumIndexContainer />
+            <div className='discover-container'>
+              <h1>Discover New Albums</h1>
+              <div className='discover-index'>
+                <AlbumIndexContainer />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </div> 
     )
   }
 }
