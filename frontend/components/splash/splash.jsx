ada82d134392c80a7d9aff962b4d56d69741486d
rizzacarla <rizzacarla.12@gmail.com>
2021-02-06 19:00:27 -0800

    Change the splash page a little bit

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavBarContainer from '../nav-bar/navbar_container';
 
 class Splash extends React.Component {
 
@@ -25,42 +26,45 @@ class Splash extends React.Component {
 
   render() {
     return(
-      <div className='splash-page'>
-        <nav className='left-navbar'>
-          {/* <div> */}
-            <p className='text1'>Looking for music?</p>
-            <h1 className='text2'>Start listening to the best new releases.</h1>
-            {this.handleRedirect()}
-          {/* </div> */}
-        </nav>
-        <nav>
-          <ul className='photos-list'>
-            <li className='flex-item'>
-              <img src={window.splash2} className="june"></img>
-              {/* <i class="material-icons">&#xe039;</i> */}
-            </li>
-            <li className='flex-item'>
-              <img src={window.splash3} className="tom"></img>
-              {/* <i class="material-icons">&#xe039;</i> */}
-            </li>
-            <li className='flex-item'>
-              <img src={window.splash4} className="surfaces"></img>
-              {/* <i class="material-icons">&#xe039;</i> */}
-            </li>
-            <li className='flex-item'>  
-              <img src={window.splash1} className="fkj"></img>
-              {/* <i class="material-icons">&#xe039;</i> */}
-            </li>
-            <li className='flex-item'>
-              <img src={window.splash6} className="eloise"></img>
-              {/* <i class="material-icons">&#xe039;</i> */}
-            </li>
-            <li className='flex-item'>
-              <img src={window.splash5} className="homemadekazoku"></img>
-              {/* <i class="material-icons">&#xe039;</i> */}
-            </li>
-          </ul>
-        </nav>
+      <div>
+        <NavBarContainer />
+        <div className='splash-page'>
+          <nav className='left-navbar'>
+            {/* <div> */}
+              <p className='text1'>Looking for music?</p>
+              <h1 className='text2'>Start listening to the best new releases.</h1>
+              {this.handleRedirect()}
+            {/* </div> */}
+          </nav>
+          <nav>
+            <ul className='photos-list'>
+              <li className='flex-item'>
+                <img src={window.splash2} className="june"></img>
+                {/* <i class="material-icons">&#xe039;</i> */}
+              </li>
+              <li className='flex-item'>
+                <img src={window.splash3} className="tom"></img>
+                {/* <i class="material-icons">&#xe039;</i> */}
+              </li>
+              <li className='flex-item'>
+                <img src={window.splash4} className="surfaces"></img>
+                {/* <i class="material-icons">&#xe039;</i> */}
+              </li>
+              <li className='flex-item'>  
+                <img src={window.splash1} className="fkj"></img>
+                {/* <i class="material-icons">&#xe039;</i> */}
+              </li>
+              <li className='flex-item'>
+                <img src={window.splash6} className="eloise"></img>
+                {/* <i class="material-icons">&#xe039;</i> */}
+              </li>
+              <li className='flex-item'>
+                <img src={window.splash5} className="homemadekazoku"></img>
+                {/* <i class="material-icons">&#xe039;</i> */}
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     )
   }
