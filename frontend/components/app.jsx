077afe23e39d8760d6f03d164d912b8a1ed64442
rizzacarla <rizzacarla.12@gmail.com>
2021-02-11 14:33:24 -0800

    Delete unnecessary containers, remove logger, and remove containers from app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,17 +9,10 @@ import LoginContainer from './login/login_form_container';
 import SplashContainer from './splash/splash_container';
 import HomePageContainer from './home/home_page_container';
 import LibraryContainer from './library/library_container';
-
-import NavBarContainer from './nav-bar/navbar_container';
-import SidebarContainer from './side-bar/sidebar_container';
 import SearchContainer from './search/search_container';
-import MusicPlayerContainer from './music-player/music_player_container';
 
-import SongsIndexContainer from './song/song_index_container';
 import SongShowContainer from './song/song_show_container';
-import ArtistsIndexContainer from './artist/artist_index_container';
 import ArtistShowContainer from './artist/artist_show_container';
-import AlbumsIndexContainer from './album/album_index_container';
 import AlbumShowContainer from './album/album_show_container';
 
 
@@ -28,11 +21,8 @@ const App = () => (
   <div className='outermost-div'>
     <Switch>
       <ProtectedRoute exact path='/albums/:albumId' component={AlbumShowContainer}></ProtectedRoute>
-      <ProtectedRoute exact path='/albums' component={AlbumsIndexContainer}></ProtectedRoute>
       <ProtectedRoute exact path='/artists/:artistId' component={ArtistShowContainer}></ProtectedRoute>
-      <ProtectedRoute exact path='/artists' component={ArtistsIndexContainer}></ProtectedRoute>
       <ProtectedRoute exact path='/songs/:songId' component={SongShowContainer}></ProtectedRoute>
-      <ProtectedRoute exact path='/songs' component={SongsIndexContainer}></ProtectedRoute>
       <ProtectedRoute exact path='/search' component={SearchContainer}></ProtectedRoute>
       <ProtectedRoute exact path='/library' component={LibraryContainer}></ProtectedRoute>
       <ProtectedRoute exact path='/home' component={HomePageContainer}></ProtectedRoute>
