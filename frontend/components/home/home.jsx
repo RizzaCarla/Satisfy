7f9d6ecf07c534083665f90f1c833bf251d5b41f
rizzacarla <rizzacarla.12@gmail.com>
2021-02-11 14:21:12 -0800

    Change home to nest child components properly

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import SongIndexContainer from '../song/song_index_container';
-import ArtistIndexContainer from '../artist/artist_index_container';
-import AlbumIndexContainer from '../album/album_index_container';
+import SongIndexComponent from '../song/song_index';
+import ArtistIndexComponent from '../artist/artist_index';
+import AlbumIndexComponent from '../album/album_index';
 import SideBarContainer from '../side-bar/sidebar_container';
 import MusicPlayerContainer from '../music-player/music_player_container';
 import NavBarContainer from '../nav-bar/navbar_container';
 
 class HomePage extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchArtists()
+      .then(() => this.props.fetchAlbums())
+      .then(() => this.props.fetchSongs())
+  }
+
   render() {
+
+    if (!this.props.albums) {
+      return null
+    }
+    if (!this.props.artists) {
+      return null
+    }
+    if (!this.props.songs) {
+      return null
+    }
+
     return (
       <div>
         <NavBarContainer />
@@ -18,19 +36,34 @@ class HomePage extends React.Component {
             <div className='discover-container'>
               <h1>Discover New Songs</h1>
               <div className='discover-index'>
-                <SongIndexContainer />
+                <SongIndexComponent 
+                  albums={this.props.albums}
+                  artists={this.props.artists}
+                  songs={Object.values(this.props.songs)}
+                  changeCurrentSong={this.props.changeCurrentSong}
+                  />
               </div>
             </div>
             <div className='discover-container'>
               <h1>Discover New Artists</h1>
               <div className='discover-index'>
-                <ArtistIndexContainer />
+                <ArtistIndexComponent 
+                  songs={this.props.songs}
+                  albums={this.props.albums}
+                  artists={Object.values(this.props.artists)}
+                  changeCurrentSong={this.props.changeCurrentSong}
+                  />
               </div>
             </div>
             <div className='discover-container'>
               <h1>Discover New Albums</h1>
               <div className='discover-index'>
-                <AlbumIndexContainer />
+                <AlbumIndexComponent 
+                  songs={this.props.songs}
+                  artists={this.props.artists}
+                  albums={Object.values(this.props.albums)}
+                  changeCurrentSong={this.props.changeCurrentSong}
+                />
               </div>
             </div>
           </div>
--- frontend/components/home/home_page_container.js
+++ frontend/components/home/home_page_container.js
@@ -1,12 +1,23 @@
 import { connect } from 'react-redux';
-import HomePage from './home'
+import HomePage from './home';
+import { fetchSongs, fetchSong, changeCurrentSong } from '../../actions/song_actions';
+import { fetchArtists, fetchArtist } from '../../actions/artist_actions';
+import { fetchAlbums, fetchAlbum } from '../../actions/album_actions';
 
-const msp = state => {
-  
-}
+const msp = state => ({
+  songs: state.entities.songs,
+  albums: state.entities.albums,
+  artists: state.entities.artists
+})
 
-const mdp = dispatch => {
+const mdp = dispatch => ({
+  fetchSongs: () => dispatch(fetchSongs()),
+  fetchSong: (id) => dispatch(fetchSong(id)),
+  fetchAlbums: () => dispatch(fetchAlbums()),
+  fetchAlbum: (id) => dispatch(fetchAlbum(id)),
+  fetchArtists: () => dispatch(fetchArtists()),
+  fetchArtist: (id) => dispatch(fetchArtist(id)),
+  changeCurrentSong: (songId) => dispatch(changeCurrentSong(songId))
+})
 
-}
-
-export default connect(null)(HomePage)
+export default connect(msp, mdp)(HomePage)
