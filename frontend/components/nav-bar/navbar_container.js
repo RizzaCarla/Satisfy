f6097702da9c3b4f6b64da597d650b4a424839b1
rizzacarla <rizzacarla.12@gmail.com>
2021-02-22 12:02:23 -0800

    Change navbar for library to have a songs tab instead of playlist

--- frontend/components/nav-bar/navbar_container.js
+++ frontend/components/nav-bar/navbar_container.js
@@ -1,32 +1,40 @@
 import NavBar from './navbar';
-import { connect } from 'react-redux';
 import { logout } from '../../actions/user_actions';
-import { setSearchQuery, clearSearchQuery } from '../../actions/search_actions'
+import { connect } from 'react-redux';
+import { setCurrentTab, clearCurrentTab } from '../../actions/library_tab_actions';
+import { setSearchQuery, clearSearchQuery } from '../../actions/search_actions';
 import { fetchSongs, fetchSong, searchSongs } from '../../actions/song_actions';
-import { fetchArtists, fetchArtist, searchArtists } from '../../actions/artist_actions';
 import { fetchAlbums, fetchAlbum, searchAlbums } from '../../actions/album_actions';
+import { fetchArtists, fetchArtist, searchArtists } from '../../actions/artist_actions';
+import { fetchLikedSongs, fetchLikedAlbums, fetchLikedArtists } from '../../actions/like_actions';
 
 const msp = state => ({
   user: state.userSession.userId,
   songs: Object.values(state.entities.songs),
   artists: Object.values(state.entities.artists),
   albums: Object.values(state.entities.albums),
-  searchQuery: state.searchReducer.searchQuery
+  searchQuery: state.searchReducer.searchQuery,
+  currentTab: state.libraryTabReducer.currentTab
 })
 
 const mdp = dispatch => ({
   logout: () => dispatch(logout()),
-  clearSearchQuery: () => dispatch(clearSearchQuery()),
-  setSearchQuery: (searchQuery) => dispatch(setSearchQuery(searchQuery)),
   fetchSongs: () => dispatch(fetchSongs()),
-  fetchArtists: () => dispatch(fetchArtists()),
   fetchAlbums: () => dispatch(fetchAlbums()),
+  fetchArtists: () => dispatch(fetchArtists()),
   fetchSong: (song) => dispatch(fetchSong(song)),
-  fetchArtist: (artist) => dispatch(fetchArtist(artist)),
   fetchAlbum: (album) => dispatch(fetchAlbum(album)),
+  clearCurrentTab: () => dispatch(clearCurrentTab()),
+  clearSearchQuery: () => dispatch(clearSearchQuery()),
+  fetchArtist: (artist) => dispatch(fetchArtist(artist)),
+  setSearchQuery: (searchQuery) => dispatch(setSearchQuery(searchQuery)),
+  setCurrentTab: (currentTab) => dispatch(setCurrentTab(currentTab)),
   searchSongs: (searchQuery) => dispatch(searchSongs(searchQuery)),
   searchArtists: (searchQuery) => dispatch(searchArtists(searchQuery)),
-  searchAlbums: (searchQuery) => dispatch(searchAlbums(searchQuery))
+  searchAlbums: (searchQuery) => dispatch(searchAlbums(searchQuery)),
+  fetchLikedSongs: (userId) => dispatch(fetchLikedSongs(userId)),
+  fetchLikedAlbums: (userId) => dispatch(fetchLikedAlbums(userId)),
+  fetchLikedArtists: (userId) => dispatch(fetchLikedArtists(userId)),
 })
 
 export default connect(msp, mdp)(NavBar);
