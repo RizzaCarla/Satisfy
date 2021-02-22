e0a7889ee52d19c9e0fbf35b9e6f98497d4591ff
rizzacarla <rizzacarla.12@gmail.com>
2021-02-22 10:19:08 -0800

    Add routes for likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'likes/show'
+    get 'likes/index'
+  end
+  namespace :api do
     get 'albums/show'
     get 'albums/index'
   end
@@ -15,11 +19,12 @@ Rails.application.routes.draw do
   get 'static/pages'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :update, :destroy, :index, :show]
-    resource :session, only: [:create, :destroy]
     resources :songs, only: [:show, :index]
     resources :albums, only: [:show, :index]
     resources :artists, only: [:show, :index]
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:create, :update, :destroy, :index, :show]
+    resources :likes, only: [:create, :update, :destroy, :index, :show]
   end
 
   root to: 'static_pages#root';
