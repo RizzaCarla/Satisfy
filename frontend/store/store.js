ce4d9912505ee577c20654d6b4c85d5404a2fec8
rizzacarla <rizzacarla.12@gmail.com>
2021-02-22 10:34:07 -0800

    temporarily change the store to have a logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
-// import logger from 'redux-logger';
-// const configureStore = (preloadedState = {}) => (
-//   createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
-// )
-
+import logger from 'redux-logger';
 const configureStore = (preloadedState = {}) => (
-  createStore(rootReducer, preloadedState, applyMiddleware(thunk))
+  createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
 )
+
+// const configureStore = (preloadedState = {}) => (
+//   createStore(rootReducer, preloadedState, applyMiddleware(thunk))
+// )
 
 export default configureStore;
