dae0733ed8fab71aa2549da68964474dfa1dd76e
rizzacarla <rizzacarla.12@gmail.com>
2021-02-28 03:52:03 -0800

    remove unnecessary reducers

--- frontend/reducers/songSession/song_session_reducer.js
+++ frontend/reducers/songSession/song_session_reducer.js
@@ -1,12 +1,8 @@
-import PauseReducer from './pause_reducer';
-import PlayReducer from './play_reducer';
 import SongIdReducer from './song_id_reducer';
 import { combineReducers } from 'redux';
 
 const songSession = combineReducers ({
   song: SongIdReducer,
-  playStatus: PlayReducer,
-  pauseStatus: PauseReducer
 })
 
 export default songSession;
