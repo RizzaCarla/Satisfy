976ef226cc34b80fb393c3697bdd7276616dcaa6
rizzacarla <rizzacarla.12@gmail.com>
2021-02-28 03:51:36 -0800

    add queue in root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,18 +1,20 @@
+import libraryTabReducer from './library/library_tab_reducer';
+import queue from './queue/queue_reducer';
 import userSession from './userSession/user_session_reducer';
 import songSession from './songSession/song_session_reducer';
-import libraryTabReducer from './library/library_tab_reducer'
-import searchReducer from './search/search_reducer'
+import searchReducer from './search/search_reducer';
 import entities from './entities/entities_reducer';
 import errors from './errors/errors_reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
+  queue,
+  errors,
   entities,
   userSession,
   songSession,
-  errors,
   searchReducer,
-  libraryTabReducer
+  libraryTabReducer,
 })
 
 export default rootReducer;
