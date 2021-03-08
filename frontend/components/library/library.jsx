7e55a7bec335403ca89e59d86268cd5deee45b6d
rizzacarla <rizzacarla.12@gmail.com>
2021-03-08 11:45:58 -0800

    Change scrollbar, add flexwrap and test the design with more songs

--- frontend/components/library/library.jsx
+++ frontend/components/library/library.jsx
@@ -4,7 +4,6 @@ import SideBarContainer from '../side-bar/sidebar_container';
 import MusicPlayerContainer from '../music-player/music_player_container';
 import LikedArtists from '../artist/artist_index_item';
 import LikedAlbums from '../album/album_index_item';
-import LikedSongs from '../song/song_index_item';
 
 class Library extends React.Component {
   constructor(props) {
@@ -44,82 +43,68 @@ class Library extends React.Component {
   render() {
     if (this.state.currentTab === "Albums") {
       return (
-        <div className='main-library-container'>
+        <div>
           <NavBarContainer />
           <SideBarContainer />
           <MusicPlayerContainer />
-          <div className='library-container'>
-            <h1>Liked Albums</h1>
-            <ul className='album-index-list'>
-              {
-                this.state.likedAlbums.map((album, index) => (
-                  <li key={index}>
-                    <LikedAlbums 
-                      album={album}
-                      albumId={album.id}
-                      albumTitle={album.album_title}
-                      albumArtistId={album.artist_id}
-                      artistName={album.artist.artist_name}
-                      albumPhotoUrl={album.albumPhotoUrl}
-                      changeCurrentSong={this.props.changeCurrentSong}
-                      songs={album.songs}
-                      />
-                  </li>
-                ))
-              }
-            </ul>
+        <div className='outer-library-container'>
+          <div className='inner-library-container'>
+            <div className='library-container'>
+              <h1>Liked Albums</h1>
+              <div>
+                <ul className='album-index-list'>
+                  {
+                    this.state.likedAlbums.map((album, index) => (
+                      <li key={index}>
+                        <LikedAlbums 
+                          album={album}
+                          albumId={album.id}
+                          albumTitle={album.album_title}
+                          albumArtistId={album.artist_id}
+                          artistName={album.artist.artist_name}
+                          albumPhotoUrl={album.albumPhotoUrl}
+                          changeCurrentSong={this.props.changeCurrentSong}
+                          songs={album.songs}
+                          />
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
       )
     } else if (this.state.currentTab === "Artists") {
       return (
-        <div className='main-library-container'>
+        <div>
           <NavBarContainer />
           <SideBarContainer />
           <MusicPlayerContainer />
-          <div className='library-container'>
-            <h1>Liked Artists</h1>
-            <ul className='artist-index-list'>
-              {
-                this.state.likedArtists.map((artist, index) => (
-                  <li key={index}>
-                    <LikedArtists
-                      artist={artist}
-                      artistId={artist.id}
-                      changeCurrentSong={this.props.changeCurrentSong}
-                      />
-                  </li>
-                ))
-              }
-            </ul>
+        <div className='outer-library-container'>
+          <div className='inner-library-container'>
+            <div className='library-container'>
+              <h1>Liked Artists</h1>
+              <div>
+                <ul className='artist-index-list'>
+                  {
+                    this.state.likedArtists.map((artist, index) => (
+                      <li key={index}>
+                        <LikedArtists
+                          artist={artist}
+                          artistId={artist.id}
+                          changeCurrentSong={this.props.changeCurrentSong}
+                          />
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      )
-    } else {
-      return (
-        <div className='main-library-container'>
-          <NavBarContainer />
-          <SideBarContainer />
-          <MusicPlayerContainer />
-          <div className='library-container'>
-            <h1>Liked Songs</h1>
-            <ul className='song-index-list'>
-              {
-                this.state.likedSongs.map((song, index) => (
-                  <li key={index}>
-                    <LikedSongs
-                      song={song}
-                      songId={song.id}
-                      songArtist={song.artist}
-                      albumPhotoUrl={song.albumPhotoUrl}
-                      changeCurrentSong={this.props.changeCurrentSong}
-                    />
-                  </li>
-                ))
-              }
-            </ul>
-          </div>
-        </div>
+      </div>
       )
     }
   }
