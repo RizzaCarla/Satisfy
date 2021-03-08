efad51b364e495e81b1aaa083385e45469ac998a
rizzacarla <rizzacarla.12@gmail.com>
2021-03-08 11:50:22 -0800

    Add liked songs page

--- frontend/components/side-bar/sidebar.jsx
+++ frontend/components/side-bar/sidebar.jsx
@@ -40,43 +40,21 @@ class Sidebar extends React.Component {
               &nbsp; 
               <li className='createplaylist'>Create Playlist</li>
             </div> */}
-            <div className='navigate-button'>
-              &nbsp; 
-              <div className='heart-bg'>
-                <i id='heart' className='fas'>&#xf004;</i>
+            <Link to='/liked-songs'>
+              <div className='navigate-button'>
+                &nbsp; 
+                <div className='heart-bg'>
+                  <i id='heart' className='fas'>&#xf004;</i>
+                </div>
+                &nbsp; 
+                &nbsp; 
+                &nbsp; 
+                <li className='likedsongs'>Liked Songs</li>
               </div>
-              &nbsp; 
-              &nbsp; 
-              &nbsp; 
-              <li className='likedsongs'>Liked Songs</li>
-            </div>
+            </Link>
             <hr className='line'/>
             {/* <ul className='playlist'>
-              <li>Anime Music</li>
-              <li>Lo-Fi</li>
-              <li>Chill-hop</li>
-              <li>Funk</li>
-              <li>Jazzy</li>
-              <li>RnB</li>
-              <li>Tagalog Songs</li>
-              <li>J-POP</li>
-              <li>K-POP</li>
-              <li>C-POP</li>
-              <li>POP</li>
-              <li>Hip Hop</li>
-              <li>Indie Rock</li>
-              <li>Christmas Music</li>
-              <li>Meme Songs</li>
-              <li>Theme Songs</li>
-              <li>OST</li>
-              <li>Classical Music</li>
             </ul> */}
-            {/* <div className='navigate-button'>
-              <i id='topside-buttons' className='far'>&#xf358;</i>
-              &nbsp; 
-              &nbsp; 
-              <li className=''>Install App</li>
-            </div> */}
           </ul>
         </div>
       )
